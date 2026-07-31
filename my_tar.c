91fee60f3d277d8b24010657e976cbff61b2e78a
ExtremePISSer <theneo247@gmail.com>
2026-07-31 18:22:08 +0200

    added other options for args

--- my_tar.c
+++ my_tar.c
@@ -48,7 +48,7 @@ Arguments parse_arguments(int argc, char const *argv[]){
     args.archiveName = NULL;
     args.indexFiles = 0;
     if(argc==1){
-        printf("erorr: no arguments present, please enter valid arguments");
+        printf("erorr: no arguments present, please enter valid arguments\n");
         exit(1);
     }
 
@@ -57,12 +57,24 @@ Arguments parse_arguments(int argc, char const *argv[]){
             for(int y = 1;argv[i][y]!='\0';y++){
                 switch (argv[i][y])
                 {
+                case 'x':
+                    printf("Extract to disk from the archive. If a file with the same name appears more than once in the archive, each copy will be extracted, with later copies overwriting (replacing) earlier copies.\n");
+                    break;
+                case 't':
+                    printf("-> List archive contents to stdout\n");
+                break;
                 case 'c':
                     args.mode = "CREATE";
                     break;
+                case 'r':
+                    printf("Like -c, but new entries are appended to the archive. The -f option is required.\n");
+                    break;
+                case 'u':
+                    printf("Like -r, but new entries are added only if they have a modification date newer than the corresponding entry in the archive. The -f option is required.\n");
+                    break;
                 case 'f':
                 if(argv[i+1]==NULL){
-                    printf("erorr: no archive name provided");
+                    printf("erorr: no archive name provided\n");
                     exit(2);
                 }
                     args.indexFiles= i+2;
@@ -71,7 +83,7 @@ Arguments parse_arguments(int argc, char const *argv[]){
                     break;
                 
                 default:
-                printf("option was not recognized");
+                printf("option was not recognized\n");
                 exit(3);
                     break;
                 }
