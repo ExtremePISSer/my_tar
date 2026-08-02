3dfa75278dc60abb6337b31418b472a25c166b17
ExtremePISSer <theneo247@gmail.com>
2026-08-02 19:21:02 +0200

    staring header creation

--- my_tar.c
+++ my_tar.c
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <fcntl.h>
 //https://www.gnu.org/software/tar/manual/html_node/Standard.html
 
 //THIS PROJECT NEEDS TO SUBMITTED AS MAKEFILE!!!!
@@ -27,16 +28,43 @@ typedef struct Arguments{
     int indexFiles;
 }Arguments;
 
+typedef struct posix_header{    /*byte offset*/
+  char name[100];               /*   0 */
+  char mode[8];                 /* 100 */
+  char uid[8];                  /* 108 */
+  char gid[8];                  /* 116 */
+  char size[12];                /* 124 */
+  char mtime[12];               /* 136 */
+  char chksum[8];               /* 148 */
+  char typeflag;                /* 156 */
+  char linkname[100];           /* 157 */
+  char magic[6];                /* 257 */
+  char version[2];              /* 263 */
+  char uname[32];               /* 265 */
+  char gname[32];               /* 297 */
+  char devmajor[8];             /* 329 */
+  char devminor[8];             /* 337 */
+  char prefix[155];             /* 345 */
+                                /* 500 */
+}posix_header;
+
 Arguments parse_arguments(int argc, char const *argv[]);
+int create_archive(Arguments args);
+int create_header(const char* filename, int archiveFd);
+
+
 int main(int argc, char const *argv[])
 {
     Arguments args = parse_arguments(argc, argv);
     //./my_tar -cf archive.tar file1 file2
     //debug print:::::::::::::::::::::::::DELETE LATER
-    printf("Archive Name: %s \n",args.archiveName);
-    printf("Mode: %s \n",args.mode);
-    printf("Number of files: %d\n",args.numberOfFiles);
+    //printf("Archive Name: %s \n",args.archiveName);
+    //printf("Mode: %s \n",args.mode);
+    //printf("Number of files: %d\n",args.numberOfFiles);
+    int archiveFd = create_archive(args);
 
+    //for loop for files:
+    create_header("for_testing.c", archiveFd);  //THIS IS TEMPORARY code, later will loop trough filenames
     return 0;
 }
 
@@ -92,4 +120,25 @@ Arguments parse_arguments(int argc, char const *argv[]){
             
     }
     return args;
+}
+
+int create_archive(Arguments args){
+    int fd = open(args.archiveName, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if(fd<0){
+        return -1;
+    }
+    return fd;
+}
+int create_header(const char * filename, int archiveFd){
+    posix_header header = {0}; //Initialize every byte of the struct to zero
+    struct stat sb;
+    int statInt;
+    statInt = stat(filename,&sb);
+    if(statInt==-1){
+        return -1;
+    }
+    strcpy(header.name,filename);
+    sprintf(header.mode,"%o",sb.st_mode);
+    printf("mode = %s\n", header.mode);
+    return 0;
 }
